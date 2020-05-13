ffd76778f87ce6011fca540f8874a8808ca775e1
Kottuzik <ewsik.s@gmaill.com>
2020-05-13 11:32:06 +0300

    final style fixes

--- cympire-app/src/App.js
+++ cympire-app/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import TeamInstances from "./components/TeamInstances";
 import Loader from "./components/loader/loader";
-import './App.css';
 
 const App = () => {
   const [teamsData, setTeamsData] = useState({});
